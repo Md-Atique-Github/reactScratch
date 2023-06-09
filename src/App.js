20687d61c10658e41f46ee7c128c19fef17a8e64
Md-Atique-Github <MuhammadAtique.bscs@gmail.com>
2023-06-09 17:00:26 +0500

    reactScratch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+// import useState from react;
+import 'bootstrap/dist/css/bootstrap.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCircleCheck,faPen,faTrashCan} from '@fortawesome/free-solid-svg-icons'
+
 import './App.css';
 
 function App() {
+
+  const [toDo, settoDo] = useState([
+    {id:1 , title: 'task i' , status: false},
+    {id:2 , title: 'task ii' , status: false}
+  ]);
+
+  const[newTask, setNewTask]= useState('');
+  const[updateData, setUpdateData]= useState('');
+
+  const addTask = () => {
+
+  }
+
+  const deleteTask = (id) => {
+    
+  }
+
+  const markDone = (id) => {
+    
+  }
+
+  const updateTask = () => {
+    
+  }
+
+  const cancelUpdate = () => {
+    
+  }
+
+  const changeTask = (e) => {
+    
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="ContainerApp">
+
+    <br /><br />
+    <h2>To Do List App (ReactJS)</h2>
+    <br /><br />
+
+      {/*Display Things*/}
+
+    {toDo && toDo.length ? '' : 'No Task...'}
+
     </div>
   );
 }
